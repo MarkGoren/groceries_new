23de233b69318ad692bb5280959842324a650846
MarkGoren <gormark2001@gmail.com>
2022-08-14 12:08:29 +0300

    shopping list, groceries, total

---
+++ my-app/src/App.js
@@ -0,0 +1,49 @@
+
+import './App.css';
+import groceries from "./data/groceries.json";
+import shoppingList from "./data/shoppingList.json";
+import React, { useState } from "react";
+
+function App() {
+  const [myGroceries, setMyGroceries] = useState(groceries)
+  const [myShoppingList, setMyShoppingList] = useState(shoppingList)
+
+  return (
+    <>
+      <div className='container' style={{'display': 'flex'}}>
+        <div className='card' style={{'width': '25rem'}}>
+          <div class="card-header">
+            Groceries
+          </div>
+
+          <ul class="list-group list-group-flush">
+            {myGroceries.map(product => <li class="list-group-item">
+              <img src={product.imgSrc} alt='uh oh' style={{'width': '5rem', 'height': 'auto'}}></img>
+              <span style={{'paddingLeft': '2rem'}}>Price: {product.price}, QTY: {product.quantity}</span>
+            </li>)}
+          </ul>
+        </div>
+
+        <div className='card' style={{'width': '25rem', 'marginLeft': '15rem'}}>
+          <div class="card-header">
+            Shopping List
+          </div>
+
+          <ul class="list-group list-group-flush">
+            {myShoppingList.length ? (myShoppingList.map(product => <li class="list-group-item">
+              <img src={product.imgSrc} alt='uh oh' style={{'width': '5rem', 'height': 'auto'}}></img>
+              <span style={{'paddingLeft': '2rem'}}>Price: {product.price}, QTY: {product.quantity}</span>
+            </li>)) : <p>Nothing Here Yet</p>}
+          </ul>
+        </div>
+      </div>
+
+      <div className='container' style={{'margin': '2.5rem 0 0 35rem'}}>
+        <p>Total Items: {myShoppingList.map(product => product.quantity).reduce((a, b) => a + b, 0)}, Price: {myShoppingList.map(product => product.quantity * product.price).reduce((a, b) => a + b, 0)}</p>
+        <button type='button' className='btn btn-success' style={{'marginLeft': '2rem'}}>Checkout</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
